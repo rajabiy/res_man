4044ac03c99c8970e5175b43a7938a73d3432167
aminiy <aminiy@localhost>
2017-02-26 22:54:51 +0500

    +Qt GUI

--- model/load.py
+++ model/load.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class ResourceLoad(type):
 
     def __init__(cls, name, bases, attrs):
@@ -8,37 +11,27 @@ class ResourceLoad(type):
             cls.register_class(cls)
 
     def register_class(cls, klass):
-        cls.load[klass.name] = klass
+        cls.load[klass.job.get('name')] = klass()
 
 
 class Application(metaclass=ResourceLoad):
-    pass
+    job = dict(name='TEMP', CPU=0, HDDIO=0,
+               RAM=0, HDD=0, priority=0)
+
+    def need(self, resource):
+        return randint(0, self.job.get(resource, 0))
 
 
 class OS(Application):
-    name = 'OS'
-    CPU = 400
-    HDDIO = 1000
-    RAM = 78
-    HDD = 7000
-    priority = 0
+    job = dict(name='OS', CPU=4e8, HDDIO=2e3,
+               RAM=7e2, HDD=7e3, priority=0)
 
 
 class App1(Application):
-    name = 'name.exe'
-    CPU = 4000
-    INS_C = 60000
-    HDDIO = 1000
-    RAM = 78
-    HDD = 7000
-    priority = 1
+    job = dict(name='app1.exe', CPU=4e8, HDDIO=2e3,
+               RAM=4e2, HDD=1e3, priority=1)
 
 
 class App2(Application):
-    name = 'ppd.exe'
-    CPU = 40000
-    INS_C = 72500
-    HDDIO = 1000
-    RAM = 78
-    HDD = 7000
-    priority = 2
+    job = dict(name='app2.exe', CPU=2e8, HDDIO=2e2,
+               RAM=6e2, HDD=1e3, priority=1)
--- model/resource.py
+++ model/resource.py
@@ -8,7 +8,7 @@ class ResourceMount(type):
             cls.register_class(cls)
 
     def register_class(cls, klass):
-        cls.resources[klass.name] = klass
+        cls.resources[klass.name] = klass()
 
 
 class Resource(metaclass=ResourceMount):
@@ -17,45 +17,59 @@ class Resource(metaclass=ResourceMount):
     measure = 'TEMP'
     count = 1
     periodical = False
+    used_quantum = 0
 
+    @property
     def full_quantum(self):
         return self.quantum * self.count
+
+    @property
+    def left_quantum(self):
+        return self.quantum * self.count - self.used_quantum
+
+    @property
+    def load(self):
+        return self.used_quantum / self.quantum * 100
+
+    def realise_quantum(self, quantum=0):
+        if self.periodical and quantum == 0:
+            self.used_quantum = 0
+        else:
+            self.used_quantum -= quantum
+
+    def get_resource(self, quantum):
+
+        if self.left_quantum - quantum < 0:
+            self.used_quantum += self.left_quantum
+            return self.left_quantum
+        else:
+            self.used_quantum += quantum
+            return quantum
 
 
 class CPU(Resource):
     name = 'CPU'
-    quantum = 1000
+    quantum = 2e9
     measure = 'FLOPS'
     count = 2
     periodical = True
 
-    def full_quantum(self):
-        return self.quantum * self.count
-
 
 class HDDIO(Resource):
     name = 'HDDIO'
-    quantum = 1000
+    quantum = 5e5
     measure = 'IOPS'
     periodical = True
 
-    def full_quantum(self):
-        return self.quantum * self.count
-
-
+'''
 class HDD(Resource):
     name = 'HDD'
     quantum = 360000
     measure = 'MB'
-
-    def full_quantum(self):
-        return self.quantum * self.count
-
+'''
 
 class RAM(Resource):
     name = 'RAM'
-    quantum = '256'
+    quantum = 2e3
     measure = 'MB'
-
-    def full_quantum(self):
-        return self.quantum * self.count
+    periodical = True
